04b381235c7d986e5b64ecc49a16d0ef6b4f001d
SmetDenis <denis@jbzoo.com>
2015-10-13 17:29:35 +0600

    Start

---
+++ src/CleanupRules.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace JBZoo\ComposerCleanup;
+
+class CleanupRules
+{
+    public static function getRules()
+    {
+        // Default patterns for common files
+        $docs  = 'README* CHANGELOG* FAQ* CONTRIBUTING* HISTORY* UPGRADING* UPGRADE* package* demo example examples doc docs readme*';
+        $tests = '.travis.yml .scrutinizer.yml phpunit.xml* phpunit.php test tests Tests travis';
+
+        return array(
+            // JBZoo pack
+            'jbzoo/data'        => array($docs, $tests),
+            'jbzoo/sqlbuilder'  => array($docs, $tests),
+            'jbzoo/simpletypes' => array($docs, $tests),
+
+            // Others
+            'symfony/yaml'      => array($docs, $tests),
+        );
+    }
+
+}
